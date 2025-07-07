27c8de586469d98298654069d2ff2ff5d51e776d
AnotherZer0 <cdtingle@gmail.com>
2025-07-07 13:36:26 -0500

    added sys functionality for main,py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import stats
+import sys
 
 #main loop
-filepath = 'books/frankenstein.txt'
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    filepath = sys.argv[1]
 
-
-print('============ BOOKBOT ============')
-print(f"Analyzing book found at {filepath}...")
-print('----------- Word Count ----------')
-print(f'Found {stats.count_book_words(filepath)} total words')
-print('--------- Character Count -------')
-for char in stats.count_chars_and_sort(filepath):
-    if char['char'].isalpha():
-        print(f"{char['char']}: {char['num']}")
-print('============= END ===============')
+    print('============ BOOKBOT ============')
+    print(f"Analyzing book found at {filepath}...")
+    print('----------- Word Count ----------')
+    print(f'Found {stats.count_book_words(filepath)} total words')
+    print('--------- Character Count -------')
+    for char in stats.count_chars_and_sort(filepath):
+        if char['char'].isalpha():
+            print(f"{char['char']}: {char['num']}")
+    print('============= END ===============')
